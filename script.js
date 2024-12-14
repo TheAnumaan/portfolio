853c8b2f63dd6555ff2be0180b020a8cb4695d8f
ANUMAAN WHIG <aanumaanwhig@gmail.com>
2024-12-14 11:13:05 +0530

    final

---
+++ script.js
@@ -0,0 +1,12 @@
+const github=document.getElementById('1')
+const instagram=document.getElementById('2')
+const twitter=document.getElementById('3')
+github.addEventListener('click',()=>{
+    window.location.href = "https://github.com/TheAnumaan";
+})
+instagram.addEventListener('click',()=>{
+    window.location.href = "https://github.com/TheAnumaan";
+})
+twitter.addEventListener('click',()=>{
+    window.location.href = "https://github.com/TheAnumaan";
+})
